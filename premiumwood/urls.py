e5f36da43340b7044d36d80416aa19087d3d7e31
katalyzator <web.coder96@gmail.com>
2017-03-09 20:34:33 +0600

    create project

---
+++ premiumwood/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+from premiumwood import settings
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
